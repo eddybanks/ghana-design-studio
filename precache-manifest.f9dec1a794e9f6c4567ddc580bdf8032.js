b5d285a899c3c9c974bf268f942ac5ca7285354b
= <=>
2019-10-08 10:06:57 -0600

    Updates

---
+++ precache-manifest.f9dec1a794e9f6c4567ddc580bdf8032.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f3d1564a4501db54eea3a41f53983c8",
+    "url": "/ghana-design-studio/index.html"
+  },
+  {
+    "revision": "95653cfcce5c446c9ffa",
+    "url": "/ghana-design-studio/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "40912754d72bdea5c4e1",
+    "url": "/ghana-design-studio/static/js/2.79bb7238.chunk.js"
+  },
+  {
+    "revision": "95653cfcce5c446c9ffa",
+    "url": "/ghana-design-studio/static/js/main.03e64385.chunk.js"
+  },
+  {
+    "revision": "abc16d236873d208f0f0",
+    "url": "/ghana-design-studio/static/js/runtime-main.49dbf28a.js"
+  }
+]);
