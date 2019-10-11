576ceec3a794d4cb6fdcdfabb842da7de56b8e77
= <=>
2019-10-10 22:57:09 -0600

    Add Gridlists and GridListTiles to landing page

--- src/containers/LandingContainer.js
+++ src/containers/LandingContainer.js
@@ -1,21 +1,44 @@
 import React, { Component } from 'react'
-import { Paper, Typography, Card } from '@material-ui/core'
+import { Paper, Typography, GridList, GridListTile, GridListTileBar } from '@material-ui/core'
 import { withStyles } from '@material-ui/core/styles'
+import { createMuiTheme } from '@material-ui/core/styles';
+import blue from '@material-ui/core/colors/blue';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: { light: 'rgba(255,252,250,0.2)', main: 'rgba(255,252,250,0.5)', dark: 'rgba(255,252,250,0.8)' },
+    someColor: { light: 'rgba(0,0,0,0.2)', main: 'rgba(0,0,200,0.5)', dark: 'rgba(0,0,0,0.7)' }
+  },
+});
 
 const styles = {
   background: {
-    backgroundColor: 'rgba(0,0,0,0.4)',
+    backgroundColor: theme.palette.someColor.dark,
     color: 'white',
-    padding: '5%',
-    margin: '2%',
-    fontSize: '3em'
+    padding: '2em',
+    margin: '2em',
+    fontSize: '3em',
+    height: '38vh',
+    textAlign: 'center'
   },
-  card: {
-    fontSize: '0.3em',
-    height: '40vh',
-    width: '20vw',
-    padding: '2rem',
-    margin: '2rem'
+  tilebar: {
+    height: "20vh",
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden'
+  },
+  gridList: {
+    flexWrap: 'nowrap',
+    // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
+    transform: 'translateZ(0)'
+  },
+  title: {
+    color: theme.palette.primary.dark,
+  },
+  titleBar: {
+    background:
+      'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
   }
 }
 
@@ -28,7 +51,8 @@ class LandingContainer extends Component {
       categories: [
         { title: "Web Design" },
         { title: "Logo Design" },
-        { title: "T-shirt Print" }
+        { title: "T-shirt Print" },
+        { title: "Hey Hey" }
       ]
     }
   }
@@ -37,10 +61,14 @@ class LandingContainer extends Component {
     const {classes} = this.props
     return (
       <Paper className={classes.background}>
-        <Typography>{this.state.title}</Typography>
-        {this.state.categories.map(item => (
-          <Card className={classes.card}>{item.title}</Card>
-        ))}
+        <Typography variant="h3">{this.state.title}</Typography><hr />
+        <GridList className={classes.gridList}>
+          {this.state.categories.map(tile => (
+            <GridListTile>
+              <GridListTileBar title={tile.title} classes={{root: classes.tilebar, title: classes.title}} />
+            </GridListTile>
+          ))}
+        </GridList>
       </Paper>
     )
   }
